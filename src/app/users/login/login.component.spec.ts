edd28fda30034aed01e29f99a298795404b48639
Shabeer Ayar <shabeer.ayar@gmail.com>
2017-03-21 12:45:08 +0530

    fix karma test

--- src/app/users/login/login.component.spec.ts
+++ src/app/users/login/login.component.spec.ts
@@ -2,8 +2,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
+import { Router } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from '../../app-routing.module';
+import { UserService } from '../shared/user.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -11,7 +18,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [HttpModule,FormsModule,RouterTestingModule],
+      declarations: [ LoginComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [UserService,{provide: APP_BASE_HREF, useValue: '/'}],
     })
     .compileComponents();
   }));
--- src/app/users/register/register.component.spec.ts
+++ src/app/users/register/register.component.spec.ts
@@ -2,16 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './register.component';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from '../shared/user.service';
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      imports: [HttpModule,FormsModule,RouterTestingModule],
+      declarations: [ RegisterComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [UserService,{provide: APP_BASE_HREF, useValue: '/'}],
     })
     .compileComponents();
   }));
